2fc5f5d1c65b62901c04b9fec96a024a854555b3
Gauri Kumari <gauri181107@gmail.com>
2026-08-17 16:24:49 +0530

    Remove javasciptbasic project

--- javasciptbasic/function.js
+++
@@ -1,9 +0,0 @@
-function calaculateTotal(price, seatCount) {
-    const total = price * seatCount;
-    return total;
-}
-
-const amountForTwoSeats = calaculateTotal(250, 2);
-console.log(`total amount : rs. ${amountForTwoSeats}`);
-
-console.log(calaculateTotal(250,2));
